65f489e81c7cb1069abfc75b13ad7bb09dc81470
Sharrine8 <65632872+Sharrine8@users.noreply.github.com>
2024-09-05 16:36:47 -0500

    refactor: app.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,9 @@
-import { useState } from "react";
 import "./App.css";
 import Header from "./Header";
 import Main from "./Main";
 import Animal from "./Animal";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Header />
